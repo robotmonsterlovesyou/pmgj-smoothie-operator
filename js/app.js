8050889ab15b739d6108b3c9a3e2f0e519d88d8b
Scott Doxey <neogeek0101@gmail.com>
2015-03-29 03:39:06 -0400

    Renamed files.

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 define(function (require) {
 
     var game = require('./game/game'),
-        intro = require('./game/intro'),
-        level = require('./game/level-truck');
+        title = require('./game/title'),
+        level = require('./game/level');
 
     game.start(level);
 
