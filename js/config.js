500a7e70c55c01483351a805dbaf704f211818c8
Scott Doxey <neogeek0101@gmail.com>
2015-03-31 00:32:57 -0400

    Removed files from bower packages.

--- js/config.js
+++ js/config.js
@@ -2,12 +2,12 @@ require.config({
     urlArgs: 'bust=' + (new Date()).getTime(),
     paths: {
 
-        'box2dweb': '../bower_components/facadejs-Box2D-plugin/vendor/box2dweb/Box2dWeb-2.1.a.3',
-        'facade': '../bower_components/facade.js/facade',
+        'box2dweb': '../bower_components/facadejs-Box2D-plugin/vendor/box2dweb/Box2dWeb-2.1.a.3.min',
+        'facade': '../bower_components/facade.js/facade.min',
         'facadejs-Box2D-plugin': '../bower_components/facadejs-Box2D-plugin/facadejs-Box2D',
         'gamepad': '../bower_components/gamepad.js/gamepad.min',
         'howler': '../bower_components/howler.js/howler.min',
-        'plastick': '../bower_components/plastick.js/plastick',
+        'plastick': '../bower_components/plastick.js/plastick.min',
         'TWEEN': '../bower_components/tween.js/build/tween.min'
     },
     shim: {
